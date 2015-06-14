ef7b11de24bc76fe333f81fd3243e089326abfeb
Jason Ou <jasonou95@gmail.com>
2015-06-13 21:00:55 -0700

    Added Projects for Cal Hacks and Hack UCSC

---
+++ Lettergram/config/routes.rb
@@ -0,0 +1,72 @@
+Rails.application.routes.draw do
+  
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'step_one', to: 'letter#new', as: 'step_one'
+  get 'step_two_gallery', to: 'gallery#new', as: 'step_two_gallery'
+  get 'step_two_design', to: 'design#new', as: 'step_two_design'
+  get 'step_three', to: 'message#new', as: 'step_three'
+  get 'step_four', to: 'charges#new', as: 'step_four'
+  
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+    root 'welcome#index'
+      resources :users
+      resources :sessions
+      resources :letter
+      resources :gallery
+      resources :design
+      resources :message
+      resources :charges
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+end
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hackathons
-Hackathon Projects
+Hackathon Projects:
+- HackUCSC:Farmable (Android app that serves as a reference for users to establish an agricultural farm in their backyard)
+- CalHacks:Lettergram (Web app used to create personalized greeting cards online and sends a physical copy to the user through mail)
